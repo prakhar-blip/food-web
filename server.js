a942d31d02d41546350ec4b7cd4e8253a50dbde6
Prakhar Sharma <147980474+prakhar-blip@users.noreply.github.com>
2024-05-23 21:27:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ import foodRouter from './routes/foodRoute.js';
 import userRouter from './routes/userRoute.js';
 import cartRouter from './routes/cartRoute.js';
 import orderRouter from './routes/orderRoute.js';
-import donationRouter from './routes/donationRoute.js';  // Import the donation router
+import donationRouter from './routes/donationRoute.js';  
 import 'dotenv/config';
 
 // app config
@@ -14,14 +14,18 @@ const port = 4000;
 
 // middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: ['https://food-thrillie.netlify.app', 'https://admin-panel-fd.vercel.app'] // Allow requests from both local frontend URLs
+}));
 
 // db connection
 connectDB();
 
+// Serve static files from the 'uploads' directory
+app.use('/images', express.static('uploads'));
+
 // api endpoint
 app.use('/api/food', foodRouter);
-app.use('/images', express.static('uploads'));
 app.use('/api/user', userRouter);
 app.use('/api/cart', cartRouter);
 app.use('/api/order', orderRouter);
